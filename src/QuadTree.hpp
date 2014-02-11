de1059abf353043ccb0aec59427bfd7378b05d35
VoxTrek <yannick.semail@gmail.com>
2014-02-11 11:50:57 +0000

    1

--- src/QuadTree.hpp
+++ src/QuadTree.hpp
@@ -1,1 +1,30 @@
 #pragma once
+
+class Object
+{
+	public:
+		int ID;
+};
+
+
+class Area
+{
+	public:
+	float x,y;
+	float w,h;
+};
+
+class QuadTree
+{
+	public:
+		int north;
+		int south;
+		int west;
+		int east;
+		
+	QuadTree *nw, *ne, *sw, *se;
+	
+	void add(Object object, Area area);
+	
+
+};
